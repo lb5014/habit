a3a47b350784d5adc77b772bd58fcc844ec64778
lb5014 <lb5014@naver.com>
2025-10-09 14:26:09 +0900

    코드 수정

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import SignupForm from '../components/SignupForm';
-import ThemeToggle from '../components/ThemeToggle';
 import { useNavigate } from 'react-router-dom';
 
 const SignupPage: React.FC = () => {
   const navigate = useNavigate();
   return (
     <div className="auth-page">
-      <div className="theme-toggle-container">
-        <ThemeToggle />
-      </div>
       <SignupForm onSwitchToLogin={() => navigate('/login')} />
     </div>
   );
